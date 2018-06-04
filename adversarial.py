4a2d0d6e398379ad3522ee41e8cacc492d0428c9
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2018-06-04 18:03:55 -0400

    imlementing adversarial training, visualizations

--- adversarial.py
+++ adversarial.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 from tqdm import tqdm
 import numpy as np
     
-from boundary import build_boundary_set_ex, build_boundary_tree_ex, build_boundary_tree
+from boundary import build_boundary_set_ex
 from common import *
 
 def bi(inits, size):
     #return tf.Variable(inits * tf.ones([size]), name='b')
     with tf.variable_scope(tf.get_default_graph().get_name_scope(), reuse=tf.AUTO_REUSE):
-        return tf.get_variable('b', size, initializer=tf.zeros_initializer)
+        return tf.get_variable('var_b', size, initializer=tf.zeros_initializer)
 
 def wi(shape):
     #init_val = np.random.normal(size=shape)*0.01
     #init_val = np.random.normal(size=shape)/math.sqrt(shape[0])
     #return tf.Variable(init_val, dtype='float', name='W')
     with tf.variable_scope(tf.get_default_graph().get_name_scope(), reuse=tf.AUTO_REUSE):
-        return tf.get_variable('W', shape)
+        return tf.get_variable('var_W', shape)
 
 def log(ss=''):
     nscp = tf.get_default_graph().get_name_scope()
@@ -48,14 +48,120 @@ def create_fcnet(acts_p, layers, inner_actvn_fn, last_actvn_fn):
             theta_lst.extend(theta)
             acts_p = acts
             
+    log('Variables in graph: %d'%(len(tf.trainable_variables())))
     return last_actvn_fn(logits), logits, theta_lst
 
+chkpts = [200, 600]
+
+class BoundaryModel:
+    def __init__(self, dim_x, dim_r, dim_t, layers, adv_train, actvn_fn, sigma, start_rate, regularizer, batch_size_bnd, epsilon_val, stop_grad):
+    
+        self.batch_size_bnd = batch_size_bnd
+        self.epsilon_val = epsilon_val
+        
+        self.epsilon = tf.placeholder(tf.float32, shape=[], name='epsilon')
+        self.X_L = tf.placeholder_with_default(tf.zeros([0,dim_x], tf.float32), shape=(None, dim_x), name='X_L')
+        self.R_L = tf.placeholder_with_default(tf.zeros([0,dim_r], tf.float32), shape=(None, dim_r), name='R_L')
+        
+        self.X_B = tf.placeholder_with_default(tf.zeros([0,dim_x], tf.float32), shape=(None, dim_x), name='X_B')
+        self.R_B = tf.placeholder_with_default(tf.zeros([0,dim_r], tf.float32), shape=(None, dim_r), name='R_B')
+        
+        num__L, num__B = tf.shape(self.X_L)[0], tf.shape(self.X_B)[0]
+        __L, __B = lambda(dat): dat[:num__L], lambda(dat): dat[num__L:]
+        
+        def classifier(X_L, R_L, X_B, R_B):
+            X = tf.concat([X_L, X_B], axis=0, name='X')
+            R = tf.concat([R_L, R_B], axis=0, name='R')
+            
+            with my_name_scope('classifier'):
+                T, T_logits, theta_T = create_fcnet(X, layers+[dim_t], tf.nn.relu, actvn_fn)
+                #print(theta_T)
+                
+            with my_name_scope('projection'):
+                T_L, T_B = __L(T), __B(T)
+                dists2 = pdist2(T_L, T_B)
+                smax = tf.nn.softmax(-dists2/sigma)
+                R_hat_T = tf.matmul(smax, R_B)
+                
+            with my_name_scope('classifier'):
+                cor = tf.clip_by_value(R_hat_T,1e-8,1.0) # self.R_hat_T + 1e-8 #
+                print('make this addition')
+                ttf = R_L * tf.log(cor)
+                loss_label = -tf.reduce_mean(ttf)
+                err = error_calc(R_L, R_hat_T)
+                bsize = tf.shape(X_B)[0]
+                
+            return R_hat_T, loss_label, err, bsize, T_L, T_B
+
+        R_hat_T, loss_label, self.err, bsize, T_L, T_B = classifier(self.X_L, self.R_L, self.X_B, self.R_B)
+        grads_wrt_input = tf.gradients(loss_label, self.X_L)[0]
+        peturb = self.epsilon*tf.sign(grads_wrt_input)
+        X_L_tilde = tf.clip_by_value(self.X_L + peturb, 0., 1.)
+        X_L_tilde = tf.stop_gradient(X_L_tilde) if stop_grad else X_L_tilde
+        R_hat_T_tilde, loss_label_tilde, self.err_tilde, bsize_tilde, T_L_tilde, T_B_tilde = classifier(X_L_tilde, self.R_L, self.X_B, self.R_B)
+        
+        self.im_X, self.im_peturb, self.im_X_tilde = self.X_L, peturb, X_L_tilde
+        T_L, T_B, T_L_tilde, R_hat_T_tilde, X_L_tilde, self.err_tilde, self.err = tf.identity(T_L, name='T_L'), tf.identity(T_B, name='T_B'), tf.identity(T_L_tilde, name='T_L_tilde'), tf.identity(R_hat_T_tilde, name='R_hat_T_tilde'), tf.identity(X_L_tilde, name='X_L_tilde'), tf.identity(self.err_tilde, name='err_tilde'), tf.identity(self.err, name='err')
+        
+        W2_ll = [tf.reduce_mean(tf.square(vv)) for vv in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if 'var_W' in vv.name]
+        loss_total = loss_label + (loss_label_tilde if adv_train else 0.) + regularizer*tf.add_n(W2_ll)
+            
+        # optimizing related code
+        smr_tr, smr_ts = [], []
+        with my_name_scope('testing'):
+            smr_scl('error', self.err, smr_ts)
+            smr_scl('error_tilde', self.err_tilde, smr_ts)
+            smr_scl('bsize', bsize_tilde, smr_ts)
+
+        with my_name_scope('training'):
+            smr_scl('error', self.err, smr_tr)
+            smr_scl('loss', loss_total, smr_tr)
+            self.sub_list = []
+            learning_rate = tf.Variable(start_rate, trainable=False)
+            smr_scl('learning_rate', learning_rate, smr_ts)
+            self.sub_list.append(RateUpdater(start_rate, learning_rate, chkpts))
+            self.opt = tf.train.AdamOptimizer(learning_rate, beta1=.5).minimize(loss=loss_total)
+            
+        self.summary_train_op = tf.summary.merge(smr_tr)
+        self.summary_test_op = tf.summary.merge(smr_ts)
+        
+    def eval_trans(self, sess, X):
+        return sess.run(tf.get_default_graph().get_tensor_by_name('T_L:0'), {self.X_L:X})
+               
+    def on_new_epoch(self, sess, last_epoch, num_epochs):
+        for it in self.sub_list:
+            it.on_new_epoch(sess, last_epoch, num_epochs)
+
+    def update_set(self, sess, X, R):
+        T = self.eval_trans(sess, X)
+        _, pts = build_boundary_set_ex(T, R)
+        self.bset = (X[pts], R[pts])
+
+    def train_step(self, sess, add_summary, i, X, R):
+        X_L, X_B, R_L, R_B = X, self.bset[0], R, self.bset[1]
+        feed_dict = {self.X_L:X_L, self.X_B:X_B, self.R_L:R_L, self.R_B:R_B, self.epsilon:self.epsilon_val}
+        sess.run(self.opt, feed_dict=feed_dict)
+        
+        if i%500: add_summary(sess.run(self.summary_train_op, feed_dict=feed_dict), i)
+
+    def on_train(self, sess, add_summary, i, X, R):
+        self.update_set(sess, X[:self.batch_size_bnd], R[:self.batch_size_bnd])
+        self.train_step(sess, add_summary, i,
+            X[self.batch_size_bnd:], R[self.batch_size_bnd:])
+
+    def on_test(self, sess, add_summary, i, X, R):
+        X_L, X_B, R_L, R_B = X, self.bset[0], R, self.bset[1]
+        feed_dict = {self.X_L:X_L, self.X_B:X_B, self.R_L:R_L, self.R_B:R_B, self.epsilon:self.epsilon_val}
+        add_summary(sess.run(self.summary_test_op, feed_dict=feed_dict), i)
+        
                 
 class BaselineModel:
-    def __init__(self, dim_x, dim_r, dim_t, layers, adv_train, start_rate):
+    def __init__(self, dim_x, dim_r, dim_t, layers, adv_train, start_rate, regularizer, epsilon_val, stop_grad):
+        
+        self.epsilon_val = epsilon_val
         
         # model related code
-        self.epsilon = tf.placeholder_with_default(.25, [], name='epsilon')
+        self.epsilon = tf.placeholder(tf.float32, shape=[], name='epsilon')
         self.X = tf.placeholder_with_default(tf.zeros([0,dim_x], tf.float32), shape=(None, dim_x), name='X')
         self.R = tf.placeholder_with_default(tf.zeros([0,dim_r], tf.float32), shape=(None, dim_r), name='R')
         
@@ -70,24 +176,29 @@ class BaselineModel:
                 
         R_hat, loss_label, self.err = classifier(self.X, self.R)
         grads_wrt_input = tf.gradients(loss_label, self.X)[0]
-        self.peturb = self.epsilon*tf.sign(grads_wrt_input)
-        self.X_tilde = tf.clip_by_value(self.X + self.peturb, 0., 1.)#tf.stop_gradient()
-        R_hat_tilde, loss_label_tilde, self.err_tilde = classifier(self.X_tilde, self.R)
+        peturb = self.epsilon*tf.sign(grads_wrt_input)
+        X_tilde = tf.clip_by_value(self.X + peturb, 0., 1.)
+        X_tilde = tf.stop_gradient(X_tilde) if stop_grad else X_tilde
+        R_hat_tilde, loss_label_tilde, self.err_tilde = classifier(X_tilde, self.R)
         
-        loss_total = loss_label + (loss_label_tilde if adv_train else 0.)
+        self.im_X, self.im_peturb, self.im_X_tilde = self.X, peturb, X_tilde
+        
+        W2_ll = [tf.reduce_mean(tf.square(vv)) for vv in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if 'var_W' in vv.name]
+        loss_total = loss_label + (loss_label_tilde if adv_train else 0.) + regularizer*tf.add_n(W2_ll)
             
         # optimizing related code
         smr_tr, smr_ts = [], []
-        with my_name_scope('error'):
-            smr_scl('train', self.err, smr_tr)
-            smr_scl('test_err', self.err, smr_ts)
-            smr_scl('test_err_tilde', self.err_tilde, smr_ts)
+        with my_name_scope('testing'):
+            smr_scl('error', self.err, smr_ts)
+            smr_scl('error_tilde', self.err_tilde, smr_ts)
 
         with my_name_scope('training'):
+            smr_scl('error', self.err, smr_tr)
+            smr_scl('loss', loss_total, smr_tr)
             self.sub_list = []
-            learning_rate = tf.Variable(start_rate, trainable=False, name='learning_rate')
+            learning_rate = tf.Variable(start_rate, trainable=False)
             smr_scl('learning_rate', learning_rate, smr_ts)
-            self.sub_list.append(RateUpdater(start_rate, learning_rate, [200, 600]))
+            self.sub_list.append(RateUpdater(start_rate, learning_rate, chkpts))
             self.opt = tf.train.AdamOptimizer(learning_rate, beta1=.5).minimize(loss=loss_total)
             
         self.summary_train_op = tf.summary.merge(smr_tr)
@@ -98,12 +209,12 @@ class BaselineModel:
             it.on_new_epoch(sess, last_epoch, num_epochs)
             
     def on_test(self, sess, add_summary, i, X, R):
-        err, err_tilde, summary = sess.run([self.err, self.err_tilde, self.summary_test_op], feed_dict={self.X:X, self.R:R})
+        err, err_tilde, summary = sess.run([self.err, self.err_tilde, self.summary_test_op], feed_dict={self.X:X, self.R:R, self.epsilon:self.epsilon_val})
         add_summary(summary, i)
         print(err, err_tilde)
     
     def on_train(self, sess, add_summary, i, X, R):
-        feed_dict = {self.X:X, self.R:R}
+        feed_dict = {self.X:X, self.R:R, self.epsilon:self.epsilon_val}
         sess.run(self.opt, feed_dict=feed_dict)
     
         if i%500: add_summary(sess.run(self.summary_train_op, feed_dict=feed_dict), i)
@@ -116,7 +227,7 @@ class ImageMan:
         im_len = 28
         im_count = 10
         idx = random.sample(population=range(D_T.images.shape[0]), k=im_count)
-        self.X_B, self.R_B = D_T.images[idx], D_T.labels[idx]#, np.argmax(D_T.labels, axis=1)[idx]
+        self.X_J, self.R_J = D_T.images[idx], D_T.labels[idx]#, np.argmax(D_T.labels, axis=1)[idx]
    
         def resh_(ims):
             nrows, ncols, height, width, intensity = (im_count, 1, im_len, im_len, 1)
@@ -130,15 +241,15 @@ class ImageMan:
             ims = tf.reshape(ims, (1, height*nrows, width*ncols, intensity))
             summary_test.append(tf.summary.image('images', ims, max_outputs=20))
 
-        peturb_im = .5 + model.peturb/2.
-        ims = tf.concat([resh_(model.X), resh_(peturb_im), resh_(model.X_tilde)], axis=2)
+        peturb_im = .5 + model.im_peturb/2.
+        ims = tf.concat([resh_(model.im_X), resh_(peturb_im), resh_(model.im_X_tilde)], axis=2)
         gen_ims(ims)
         #gen_ims('generated_images', model.X_hat)
         self.summary_test_op = tf.summary.merge(summary_test)
             
     def on_test(self, sess, add_summary, i, X, R):
         model = self.model
-        add_summary(sess.run(self.summary_test_op, feed_dict={model.X: self.X_B, model.R: self.R_B}), i)
+        add_summary(sess.run(self.summary_test_op, feed_dict={model.X_L: self.X_J, model.R_L: self.R_J, model.epsilon:model.epsilon_val}), i)
 
     def on_new_epoch(self, sess, last_epoch, num_epochs): pass
     def on_train(self, sess, add_summary, i, X, R): return
@@ -168,15 +279,13 @@ class Trainer:
                     module.on_new_epoch(sess, last_epoch, num_epochs)
         
         
-def make_model(modt, start_rate, sigma, batch_size_bnd, batch_size_trn, adv_train, D_L):
+def make_model(modt, dim_t, start_rate, regularizer, epsilon_val, stop_grad, sigma, batch_size_bnd, adv_train, D_L):
     if modt=='set':
         actvn_fn = tf.identity
-        model = BoundaryModel(dim_x=784, dim_r=10, dim_t=20, layers=[400,400], actvn_fn=actvn_fn, sigma=sigma)
-        optimizer = SetOptimizer(model, start_rate, batch_size_bnd, batch_size_trn, D_L)
-        return model, optimizer
-
+        return BoundaryModel(dim_x=784, dim_r=10, dim_t=dim_t, layers=[400,400], adv_train=adv_train, actvn_fn=actvn_fn, sigma=sigma, start_rate=start_rate, regularizer=regularizer, batch_size_bnd=batch_size_bnd, epsilon_val=epsilon_val, stop_grad=stop_grad)
+        
     if modt=='baseline':
-        return BaselineModel(dim_x=784, dim_r=10, dim_t=20, layers=[400,400], adv_train=adv_train, start_rate=start_rate)
+        return BaselineModel(dim_x=784, dim_r=10, dim_t=dim_t, layers=[400,400], adv_train=adv_train, start_rate=start_rate, regularizer=regularizer, epsilon_val=epsilon_val, stop_grad=stop_grad)
         
         
 reset_all()
@@ -187,14 +296,18 @@ num_epochs = 1000
 batch_size_bnd = 100
 batch_size_trn = 100
 dset = 'digits' #digits/fashion
-modt = 'baseline' #set/tree/tree_bch/baseline
+modt = 'set' #set/baseline
 start_rate = 0.001
-adv_train = 0
+regularizer = 0.001
+epsilon_val = .25
+adv_train = 1
+stop_grad = 1
+dim_t = 2
 sigma = 60
 
-run_id = '%s_%s_%dmbnd_%dmbtr_%srate_%dsigma_%dadv_train'%(dset, modt, batch_size_bnd, batch_size_trn, format_e(start_rate), sigma, adv_train)
+run_id = '%s_%s_%dmbnd_%dmbtr_%ddim_t_%srate_%sregularizer_%sepsilon_val_%dsigma_%dadv_train_%dstop_grad'%(dset, modt, batch_size_bnd, batch_size_trn, dim_t, format_e(start_rate), format_e(regularizer), str(epsilon_val), sigma, adv_train, stop_grad)
 trainer = Trainer(load_mnist(dset))
-model = make_model(modt, start_rate, sigma, batch_size_bnd, batch_size_trn, adv_train, trainer.ds.train.labeled_ds)
+model = make_model(modt, dim_t, start_rate, regularizer, epsilon_val, stop_grad, sigma, batch_size_bnd, adv_train, trainer.ds.train.labeled_ds)
 sman = SessMan(run_id=run_id, new_run=new_run, real_run=real_run)
 imageman = ImageMan(sman, model, trainer.ds.test)
 sman.load()
--- eval.py
+++ eval.py
@@ -4,13 +4,25 @@ import tensorflow as tf
 import pickle, sys, os
 import numpy as np
 
-from boundary import build_boundary_tree_ex
+from boundary import build_boundary_tree_ex, build_boundary_set_ex
 from common import *
 
 
 cache_root = os.path.join('..', 'cache')
 
 
+def get_default_defs(run_id):
+    if 'fashion' in run_id:
+        dset = 'fashion'
+        fontdict = {'fontsize':10, 'weight':"bold"}
+        defs = {0: 'T-shirt/top', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt', 7: 'Sneaker', 8: 'Bag', 9: 'Ankle boot'}
+    elif 'digits' in run_id:
+        dset = 'digits'
+        defs = dict([(i, str(i)) for i in range(10)])
+        fontdict = {'fontsize':20, 'weight':"bold"}
+    return dset, fontdict, defs
+        
+            
 def extract_events(path):
     tt, vv, ss = [], [], None
     try:
@@ -26,7 +38,9 @@ def extract_events(path):
     return np.array(tt)-ss, np.array(vv)
 
 
-def fn_time():
+def fn_time(run_id):
+    
+    #dset, fontdict, defs = get_default_defs(run_id)
     lims = [(30,50000), (10,40)]
     runs = (['20180508-00:32:28_fashion_tree_1000mbnd_1000mbtr_1E-04rate_60sigma', 'DBT'],
             ['20180504-03:34:02_fashion_set_100mbnd_100mbtr_1E-03rate_60sigma', 'DBS'])
@@ -73,40 +87,30 @@ def fn_time():
     plt.show()
 
 
-def eval_dset(X_L, cache_dir):
+def eval_dset__(cache_dir):
     ckpt = tf.train.get_checkpoint_state(cache_dir)
     assert(ckpt)
     print 'model_checkpoint: ', ckpt.model_checkpoint_path
     
-    reset_all()
     sess = tf.Session()
     saver = tf.train.import_meta_graph('%s.meta'%ckpt.model_checkpoint_path)
+    tf.set_random_seed(0)
     saver.restore(sess, ckpt.model_checkpoint_path)
     last_epoch = int(ckpt.model_checkpoint_path.split('-')[-1])
     print('Restored epoch: %d'%last_epoch)
+    return sess
 
+def eval_dset(X_L, cache_dir):
+    sess = eval_dset__(cache_dir)
     tf_T = tf.get_default_graph().get_tensor_by_name('classifier/Identity:0')
     tf_X_L = tf.get_default_graph().get_tensor_by_name('X_L:0')
     trans = sess.run(tf_T, {tf_X_L:X_L})
     return trans
 
 
-#run_id = '20180504-03:36:26_digits_set_1000mbnd_1000mbtr_1E-03rate_60sigma'
-run_id = '20180504-03:34:02_fashion_set_100mbnd_100mbtr_1E-03rate_60sigma'
-#run_id = '20180512-16:13:04_digits_set_100mbnd_100mbtr_1E-03rate_60sigma_0stop_grad_2d'
-#run_id = '20180514-08:42:14_fashion_set_100mbnd_100mbtr_1E-03rate_60sigma_0stop_grad_2d'
-
-if 'fashion' in run_id:
-    dset = 'fashion'
-    fontdict = {'fontsize':10, 'weight':"bold"}
-    defs = defs_f = {0: 'T-shirt/top', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt', 7: 'Sneaker', 8: 'Bag', 9: 'Ankle boot'}
-else if 'digits' in run_id:
-    dset = 'digits'
-    defs_d = dict([(i, str(i)) for i in range(10)])
-    fontdict = {'fontsize':20, 'weight':"bold"}
-
-
-def fn_trans():
+def fn_trans(run_id):
+    
+    dset, fontdict, defs = get_default_defs(run_id)
     cache_dir = os.path.join(cache_root, run_id)
     cache_file = os.path.join(cache_dir, 'tsne_trans')
     if 0:#os.path.exists(cache_file):
@@ -142,7 +146,9 @@ def fn_trans():
     plt.show()
 
 
-def fn_gray():
+def fn_gray(run_id):
+    
+    dset, fontdict, defs = get_default_defs(run_id)
     D_T = load_mnist(dset).train.labeled_ds #digits/fashion test#
     cache_dir = os.path.join(cache_root, run_id)
     T = eval_dset(D_T.images, cache_dir)
@@ -165,8 +171,136 @@ def fn_gray():
     plt.savefig(os.path.join(cache_root, 'tsne.pdf'), bbox_inches='tight')
     plt.show()
 
+    
+get_tensor1 = lambda arg: tf.get_default_graph().get_tensor_by_name(arg+':0')
+    
+def eval_dset_ex(inps, outs, sess):
+    feed_dict = {get_tensor1(kk):inps[kk] for kk in inps}
+    outs_tt = [get_tensor1(kk) for kk in outs]
+    return sess.run(outs_tt, feed_dict=feed_dict)
 
+def permute(items):
+    perm = np.arange(items[0].shape[0])
+    np.random.shuffle(perm)
+    return [tt[perm] for tt in items]
+
+def hide_axticks(ax):
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+    ax.axis('off')
+
+    
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from matplotlib import gridspec
+import scipy.misc
+
+def fn_movie(run_id):
+    reset_all(599544)
+    dset, fontdict, defs = get_default_defs(run_id)
+    
+    if 'fashion' in run_id:
+        dset = 'fashion'
+        fontdict = {'fontsize':6, 'weight':'bold'}
+        defs = {0: 'T-shirt/top', 1: 'Trouser', 2: 'Pullover', 3: 'Dress', 4: 'Coat', 5: 'Sandal', 6: 'Shirt', 7: 'Sneaker', 8: 'Bag', 9: 'Ankle boot'}
+    elif 'digits' in run_id:
+        dset = 'digits'
+        defs = dict([(i, str(i)) for i in range(10)])
+        fontdict = {'fontsize':10, 'weight':'bold'}
+
+    cache_dir = os.path.join(cache_root, run_id)
+    cache_file = os.path.join(cache_dir, 'tsne_trans')
+    mnist = load_mnist(dset, n_labeled=10000)
+    DS = mnist.train.labeled_ds
+    X, R = permute([DS.images, DS.labels])
+
+    sess = eval_dset__(cache_dir)
+    T, Y = eval_dset_ex({'X_L':X}, ['T_L'], sess)[0], np.argmax(R, 1)
+    assert(T.shape[1]==2)
+        
+    if 0:
+        _, pts = build_boundary_set_ex(T, R)
+        pts = np.array(pts)
+        X_B, R_B = X[pts], R[pts]
+        print('boundary size: ', X_B.shape[0])
+
+        rest = (X[~pts], R[~pts])
+        X_L, R_L = rest[0][:5], rest[1][:5]
+        print('points size: ', X_L.shape[0])
+    else:
+        ll_classes, from_each = range(10), 2
+        #ll_classes, from_each = [6], 5
+        X_B, R_B = X, R
+        rest = (mnist.test.images, mnist.test.labels)
+        inds = [it for ii in ll_classes for it in np.random.choice(np.nonzero(np.argmax(rest[1], 1)==ii)[0], from_each).tolist()]
+        X_L, R_L = rest[0][inds], rest[1][inds]
+        
+    feed_dict = {'X_L':X_L, 'R_L':R_L, 'X_B':X_B, 'R_B':R_B}
+    T_L, T_B = eval_dset_ex(feed_dict, ['T_L', 'T_B'], sess)
+    Y_L, Y_B = np.argmax(R_L, 1), np.argmax(R_B, 1)
+    
+    cmap = plt.get_cmap('tab10')
+    colors = cmap(np.linspace(0, 1, 10))
+   
+    print 'plotting!'
+    fig = plt.figure(figsize=(10,5))
+    gs = gridspec.GridSpec(1, 2, width_ratios=[3, 2])
+    ax1 = plt.subplot(gs[0])
+    
+    #background scatter
+    ax1.scatter(T_B[:, 0], T_B[:, 1], marker='o', s=5, edgecolor='none', c=Y_B, cmap=cmap)
+    
+    #generate path
+    mossaic_ll = []
+    for eps in [it*.01 for it in range(0, 30, 3)]:
+        feed_dict = {'X_L':X_L, 'R_L':R_L, 'X_B':X_B, 'R_B':R_B, 'epsilon':eps}
+        T_L_tilde, R_hat_T_tilde, X_L_tilde = eval_dset_ex(feed_dict, ['T_L_tilde', 'R_hat_T_tilde', 'X_L_tilde'], sess)
+        #plt.scatter(T_L_tilde[:, 0], T_L_tilde[:, 1], marker='P', s=100, edgecolor='none', c='k')
+        mossaic_ll.append(X_L_tilde.reshape([-1, 28, 28]).swapaxes(0,1).reshape(28, -1))
+        for coords, preds, label, tilde in zip(T_L_tilde, R_hat_T_tilde, Y_L, X_L_tilde):
+            if 1:
+                #color = np.clip(np.sqrt(np.matmul(preds**2, colors)), 0., 1.)
+                color = np.clip(np.matmul(preds, colors), 0., 1.)
+                ax1.text(coords[0], coords[1], defs[label][0], fontdict={'fontsize':6, 'weight':'bold', 'color':color})
+            else:
+                img = np.clip(tilde, 0, 1).reshape([28,28])
+                ofim = OffsetImage(img, zoom=1, cmap='gray')
+                ab = AnnotationBbox(ofim, coords, xycoords='data', frameon=False)
+                ax1.add_artist(ab)
+                #ax.update_datalim(np.column_stack([x, y]))
+                #ax.autoscale()
+
+    #cluster center label
+    for i in range(10):
+        indices = Y_B == i
+        center = np.average(T_B[indices], 0)
+        ax1.text(center[0], center[1], defs[i], fontdict=fontdict)
+
+    ax2 = plt.subplot(gs[1])
+    mossaic = np.concatenate(mossaic_ll, 0)
+    ax2.imshow(mossaic, cmap='gray')
+    #scipy.misc.toimage(, cmin=0., cmax=1.).save(os.path.join(cache_root, 'outfile.pdf'))
+
+    ax1.set_aspect('equal', adjustable='box')
+    hide_axticks(ax1)
+    hide_axticks(ax2)
+    #plt.show()
+    plt.savefig(os.path.join(cache_root, '%s.pdf'%run_id), bbox_inches='tight')
+    print('saved plot.')
+    
+    # calc final test error using all traning data
+    feed_dict = {'X_L':mnist.test.images, 'R_L':mnist.test.labels, 'X_B':mnist.train.labeled_ds.images, 'R_B':mnist.train.labeled_ds.labels, 'epsilon':.25}
+    err, err_tilde = eval_dset_ex(feed_dict, ['err', 'err_tilde'], sess)
+    print({'err':err, 'err_tilde':err_tilde})
+
+
+def fn_movies():
+    run_ll = [os.path.basename(x[0]) for x in os.walk(cache_root)]
+    return ([fn_movie(run_id) for run_id in run_ll if (('2dim_t' in run_id) and ('adv_train' in run_id) and ('_baseline_' not in run_id))])
+        
 if __name__ == '__main__':
-    if sys.argv[1]=='time': fn_time()
-    if sys.argv[1]=='trans': fn_trans()
-    if sys.argv[1]=='gray': fn_gray()
+    
+    reset_all(599)
+    if sys.argv[1]=='time': fn_time(run_id)
+    if sys.argv[1]=='trans': fn_trans(run_id)
+    if sys.argv[1]=='gray': fn_gray(run_id)
+    if sys.argv[1]=='movie': fn_movies()
