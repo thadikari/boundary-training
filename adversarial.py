193f3d5c3447aa5a04848062b517f1de1d7e79dd
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2018-06-17 10:56:41 -0400

    refactoring tf operation names, adding distance based adv regularization for bset

--- adversarial.py
+++ adversarial.py
@@ -67,44 +67,51 @@ class BoundaryModel:
         self.R_B = tf.placeholder_with_default(tf.zeros([0,dim_r], tf.float32), shape=(None, dim_r), name='R_B')
         
         num__L, num__B = tf.shape(self.X_L)[0], tf.shape(self.X_B)[0]
-        __L, __B = lambda(dat): dat[:num__L], lambda(dat): dat[num__L:]
+        __L, __B = lambda dat, nn: tf.identity(dat[:num__L], name=nn), lambda dat, nn: tf.identity(dat[num__L:], name=nn)
         
-        def classifier(X_L, R_L, X_B, R_B):
+        def classifier(X_L, R_L, X_B, R_B, suffx):
             X = tf.concat([X_L, X_B], axis=0, name='X')
             R = tf.concat([R_L, R_B], axis=0, name='R')
             
-            with my_name_scope('classifier'):
-                T, T_logits, theta_T = create_fcnet(X, layers+[dim_t], tf.nn.relu, actvn_fn)
-                #print(theta_T)
+            #with my_name_scope('classifier'):
+            T, T_logits, theta_T = create_fcnet(X, layers+[dim_t], tf.nn.relu, actvn_fn)
+            #print(theta_T)
                 
-            with my_name_scope('projection'):
-                T_L, T_B = __L(T), __B(T)
-                dists2 = pdist2(T_L, T_B)
-                smax = tf.nn.softmax(-dists2/sigma)
-                R_hat_T = tf.matmul(smax, R_B)
-                
-            with my_name_scope('classifier'):
-                cor = tf.clip_by_value(R_hat_T,1e-8,1.0) # self.R_hat_T + 1e-8 #
-                print('make this addition')
-                ttf = R_L * tf.log(cor)
-                loss_label = -tf.reduce_mean(ttf)
-                err = error_calc(R_L, R_hat_T)
-                bsize = tf.shape(X_B)[0]
+            #with my_name_scope('projection'):
+            T_L, T_B = __L(T, 'T_L'+suffx), __B(T, 'T_B'+suffx)
+            dists2 = pdist2(T_L, T_B)
+            smax = tf.nn.softmax(-dists2/sigma)
+            R_hat_T = tf.matmul(smax, R_B, name='R_hat_T'+suffx)
+            
+            #with my_name_scope('classifier'):
+            cor = tf.clip_by_value(R_hat_T,1e-8,1.0) # self.R_hat_T + 1e-8 #
+            print('make this addition')
+            ttf = R_L * tf.log(cor)
+            loss_label = -tf.reduce_mean(ttf)
+            err = error_calc(R_L, R_hat_T)
+            bsize = tf.shape(X_B)[0]
+            err = tf.identity(err, name='err'+suffx)
                 
             return R_hat_T, loss_label, err, bsize, T_L, T_B
 
-        R_hat_T, loss_label, self.err, bsize, T_L, T_B = classifier(self.X_L, self.R_L, self.X_B, self.R_B)
+        R_hat_T, loss_label, self.err, bsize, T_L, T_B = classifier(self.X_L, self.R_L, self.X_B, self.R_B, '')
         grads_wrt_input = tf.gradients(loss_label, self.X_L)[0]
         peturb = self.epsilon*tf.sign(grads_wrt_input)
         X_L_tilde = tf.clip_by_value(self.X_L + peturb, 0., 1.)
         X_L_tilde = tf.stop_gradient(X_L_tilde) if stop_grad else X_L_tilde
-        R_hat_T_tilde, loss_label_tilde, self.err_tilde, bsize_tilde, T_L_tilde, T_B_tilde = classifier(X_L_tilde, self.R_L, self.X_B, self.R_B)
+        X_L_tilde = tf.identity(X_L_tilde, name='X_L_tilde')
+        R_hat_T_tilde, loss_label_tilde, self.err_tilde, bsize_tilde, T_L_tilde, T_B_tilde = classifier(X_L_tilde, self.R_L, self.X_B, self.R_B, '_tilde')
         
         self.im_X, self.im_peturb, self.im_X_tilde = self.X_L, peturb, X_L_tilde
-        T_L, T_B, T_L_tilde, R_hat_T_tilde, X_L_tilde, self.err_tilde, self.err = tf.identity(T_L, name='T_L'), tf.identity(T_B, name='T_B'), tf.identity(T_L_tilde, name='T_L_tilde'), tf.identity(R_hat_T_tilde, name='R_hat_T_tilde'), tf.identity(X_L_tilde, name='X_L_tilde'), tf.identity(self.err_tilde, name='err_tilde'), tf.identity(self.err, name='err')
         
         W2_ll = [tf.reduce_mean(tf.square(vv)) for vv in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES) if 'var_W' in vv.name]
-        loss_total = loss_label + (loss_label_tilde if adv_train else 0.) + regularizer*tf.add_n(W2_ll)
+        weight_loss = tf.add_n(W2_ll)
+        
+        if adv_train==1: adv_loss = loss_label_tilde
+        elif adv_train==2: adv_loss = tf.reduce_mean(tf.square(tf.stop_gradient(T_L)-T_L_tilde))
+        else: adv_loss = 0.
+        
+        loss_total = loss_label + adv_loss + regularizer*weight_loss
             
         # optimizing related code
         smr_tr, smr_ts = [], []
@@ -114,13 +121,17 @@ class BoundaryModel:
             smr_scl('bsize', bsize_tilde, smr_ts)
 
         with my_name_scope('training'):
-            smr_scl('error', self.err, smr_tr)
+            smr_scl('loss_label', loss_label, smr_tr)
+            smr_scl('weight_loss', weight_loss, smr_tr)
+            smr_scl('adv_loss', adv_loss, smr_tr)
             smr_scl('loss', loss_total, smr_tr)
+            smr_scl('error', self.err, smr_tr)
             self.sub_list = []
             learning_rate = tf.Variable(start_rate, trainable=False)
             smr_scl('learning_rate', learning_rate, smr_ts)
             self.sub_list.append(RateUpdater(start_rate, learning_rate, chkpts))
-            self.opt = tf.train.AdamOptimizer(learning_rate, beta1=.5).minimize(loss=loss_total)
+            self.opt = tf.train.AdamOptimizer(learning_rate).minimize(loss=loss_total)
+            #self.opt1 = tf.train.AdamOptimizer(learning_rate/10.).minimize(loss=adv_loss)
             
         self.summary_train_op = tf.summary.merge(smr_tr)
         self.summary_test_op = tf.summary.merge(smr_ts)
@@ -141,6 +152,7 @@ class BoundaryModel:
         X_L, X_B, R_L, R_B = X, self.bset[0], R, self.bset[1]
         feed_dict = {self.X_L:X_L, self.X_B:X_B, self.R_L:R_L, self.R_B:R_B, self.epsilon:self.epsilon_val}
         sess.run(self.opt, feed_dict=feed_dict)
+        #sess.run(self.opt1, feed_dict=feed_dict)
         
         if i%500: add_summary(sess.run(self.summary_train_op, feed_dict=feed_dict), i)
 
@@ -251,7 +263,7 @@ class ImageMan:
             
     def on_test(self, sess, add_summary, i, X, R):
         model = self.model
-        add_summary(sess.run(self.summary_test_op, feed_dict={model.X_L: self.X_J, model.R_L: self.R_J, model.epsilon:model.epsilon_val}), i)
+        add_summary(sess.run(self.summary_test_op, feed_dict={model.X_L: self.X_J, model.R_L: self.R_J, model.X_B:model.bset[0], model.R_B:model.bset[1], model.epsilon:model.epsilon_val}), i)
 
     def on_new_epoch(self, sess, last_epoch, num_epochs): pass
     def on_train(self, sess, add_summary, i, X, R): return
@@ -302,7 +314,7 @@ modt = 'set' #set/baseline
 start_rate = 0.001
 regularizer = 0.001
 epsilon_val = .25
-adv_train = 1
+adv_train = 1 #1=standard FGSM / 2=nearest neigh
 stop_grad = 1
 dim_t = 2
 sigma = 60
